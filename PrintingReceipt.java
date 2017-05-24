c081fcf634afdd1122a73913db84ced3193c1cd9
glemma32 <genet.woldeyes@gmail.com>
2017-05-24 17:31:59 -0400

    Receipt printing commit

---
+++ PrintingReceipt.java
@@ -0,0 +1,40 @@
+
+public class PrintingReceipt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  /*+--------------------------------------+
+		|      Java Bank ATM Receipt           |
+		|      Wednesday, December 2, 2015     |
+		|      ATM Location # 123              |
+		|                                      |
+		|                                      |
+		|      Account Number:      1234567    |
+		|      Customer:     John Q. Public    |
+		|      Transaction Type:    Deposit    |
+		|      Transaction Amount:  $500.00    |
+		|      Account Balance:   $1,500.00    |
+		|                                      |
+		|      Thank you for banking with us   |
+		|            Have a coffee day         |
+		|                                      |
+		+--------------------------------------+*/
+		
+		System.out.println("\t" +"+"+"---------------------------------------"+""+"+");
+		System.out.println("\t" +"|"+"\t"+"Java Bank ATM Receipt"+"\t"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"Wednesday, December 2, 2015 "+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"\t"+"\t"+"\t"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"\t"+"\t"+"\t"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+" ATM Location # 123 "+"\t"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"Account Number:      1234567"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"Customer:     John Q. Public"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"Transaction Type:    Deposit "+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"Transaction Amount:  $500.00"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"Account Balance:   $1,500.00 "+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"\t"+"\t"+"\t"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"Thank you for banking with us"+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"\t"+"Have a coffee day "+"\t"+"|");
+		System.out.println("\t" +"|"+"\t"+"\t"+"\t"+"\t"+"\t"+"|");
+		System.out.println("\t" +"+"+"---------------------------------------"+""+"+");
+	}
+}
